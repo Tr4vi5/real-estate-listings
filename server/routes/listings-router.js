041939b960d59706420aa87443e766be3eb44b25
Tr4vi5 <trav.dunn@outlook.com>
2018-08-20 16:40:23 -0500

    fixed pool redundancy

--- server/routes/listings-router.js
+++ server/routes/listings-router.js
@@ -1,26 +1,6 @@
 //requires
 const express = require('express');
 const router = express.Router();
-//pg
-const pg = require('pg');
-const Pool = pg.Pool;
-const config = {
-    database: 'real-estate',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 10000
-}
-const pool = new Pool(config);
-pool.on('connect',()=>{
-    console.log('Postgresql connected');
-    
-});
-pool.on('error', (error)=>{
-    console.log('Error connecting to SQL database', error);
-    
-});
-
 const pool = require('../modules/pool.js');
 //routes
 router.get('/forsale', (req,res)=>{
